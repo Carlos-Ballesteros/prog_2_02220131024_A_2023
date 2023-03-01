b9c590ba3cce12f36f9b38a9e6e286d29dc57d3a
Carlos-Ballesteros <124361570+Carlos-Ballesteros@users.noreply.github.com>
2023-03-01 18:09:46 -0500

    Add files via upload

--- Taller_3/PTO18.java
+++ Taller_3/PTO18.java
@@ -10,6 +10,6 @@ public class PTO18 {
         String apellido1 = sc.nextLine();
         String apellido2 = sc.nextLine();
 
-        
+        System.out.println("Sus iniciales son " + nombre.charAt(0) + ". " + apellido1.charAt(0) + ". " + apellido2.charAt(0));
     }
 }
